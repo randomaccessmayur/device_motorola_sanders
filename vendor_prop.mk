1e05952160c6c725809fba634bac0c6d873b071c
mendigodepijama <mendigodepijama@gmail.com>
2019-07-13 02:21:17 -0300

    Sanders: gps: Tweaks to fix GPS [SQUASHED]
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
    
    Co-authored-by: marshmello61 <ultramayur123@gmail.com>
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -395,20 +395,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.config.swap=true
 
-# Properties
+# GPS
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.fha_enable=true \
-    ro.sys.fw.bg_apps_limit=32 \
-    ro.config.dha_cached_max=16 \
-    ro.config.dha_empty_max=42 \
-    ro.config.dha_empty_init=32 \
-    ro.config.dha_lmk_scale=0.545 \
-    ro.config.dha_th_rate=2.3 \
-    ro.config.sdha_apps_bg_max=64 \
-    ro.config.sdha_apps_bg_min=8
-
-#Device Model
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.model=Moto G (5S) Plus \
-    ro.product.vendor.model=Moto G (5S) Plus \
-    ro.build.product=Moto G (5S) Plus
+    ro.location.osnlp.region.package=com.google.android.gms
