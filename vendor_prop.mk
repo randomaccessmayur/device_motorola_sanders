528a9b08f0faf2ee14999575a2cb9d0ad25fb982
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-08-18 01:51:49 +0530

    Sanders: prop: add some unintentionally removed props
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -390,6 +390,18 @@ persist.radio.data_lte_sys_ind=1
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.config.swap=true
 
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.location.osnlp.region.package=com.google.android.gms
