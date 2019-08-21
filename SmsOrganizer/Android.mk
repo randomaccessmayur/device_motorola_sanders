b438aacf6b5848aae670f4e3a45f9fbe37b80bbb
marshmello61 <ultramayur123@gmail.com>
2019-08-21 05:58:11 +0000

    Sanders: sms: add smsorganizer
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

---
+++ SmsOrganizer/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SmsOrganizer
+LOCAL_SRC_FILES := priv-app/SmsOrganizer/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDE_PACKAGES := Messaging Messages messaging
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
