8cf2ccaa89d6c4bd0798edfc47968bd4e061d65f
marshmello61 <ultramayur123@gmail.com>
2019-08-24 02:03:05 +0000

    Sanders: prebuilts: Add prebuilt ActionServices
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>
    
    Rename ActionServices -> MatchmakerPrebuilt
    
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

---
+++ MatchmakerPrebuilt/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := MatchmakerPrebuilt
+LOCAL_SRC_FILES := priv-app/MatchmakerPrebuilt/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
--- device.mk
+++ device.mk
@@ -485,6 +485,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SmsOrganizer
 
+# ActionServices
+PRODUCT_PACKAGES += \
+    MatchmakerPrebuilt
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/MatchmakerPrebuilt/privapp-permissions-actionservices.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-actionservices.xml
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
